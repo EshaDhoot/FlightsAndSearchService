74fae225afaa39ef002d82252e2dd526699453b2
Esha <maheshwariesha11@gmail.com>
2024-01-06 20:19:43 +0530

    Added routers to complete create API

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,6 +1,6 @@
-const { City } = require('../models/city');
+const { City } = require('../models/index');
 
-class  CityRepository{
+class  CityRepository {
     async createCity({ name }){
         try {
             const city = await City.create({ name });
