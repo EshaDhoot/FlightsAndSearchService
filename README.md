85c63899a801863155a70fbb9fe28eee0479a54f
Esha <maheshwariesha11@gmail.com>
2024-01-07 16:50:28 +0530

    Added airport seeders

--- README.md
+++ README.md
@@ -33,8 +33,10 @@
 
 ### City -> id, name, created_at, updated_at
 ### Airport -> id, name, address, city_id, created_at, updated_at
-    Relationship => City has many airports and Airport belongs to a city (1:N)
+    Relationship => City has many airports and Airport belongs to a city (1:N) 
 
+```npx sequelize model:generate --name Airport --attributes name:String,address:String,cityId:integer```
+```npx sequelize seed:generate --name add-airports```
 
 ## Project Structure
 / 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig')
 const ApiRoutes  = require('./routes/index');
 
+const db = require('./models/index');
+
 const setupAndStartServer = async () =>{
     
     // create the express object
@@ -16,8 +18,10 @@ const setupAndStartServer = async () =>{
 
     app.listen(PORT,() => {
         console.log("Server started at PORT",PORT);
-        
-    }) 
+        if(process.env.SYNC_DB) {
+            db.sequelize.sync({alter: true});
+        }
+    });
 
 }
 
